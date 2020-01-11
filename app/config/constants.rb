b8cafcb35b32f64c05ea02aae545d348ca7a6d79
donny741 <ddonatasjar@gmail.com>
2020-01-11 17:25:52 +0200

    fix constants

--- app/config/constants.rb
+++ app/config/constants.rb
@@ -2,5 +2,5 @@
 
 SCALE = ENV.fetch('SCALE') { 1 }
 DEFAULT_FONT = 'app/assets/fonts/Public_Sans/PublicSans-VariableFont:wght.ttf'
-MAX_TICK = 720
-ICAL_URL = ENV.fetch('ICAL_URL')
+MAX_TICK = ENV.fetch('MAX_TICK') { 720 }
+ICAL_URL = ENV.fetch('ICAL_URL') { 'changeme' }
